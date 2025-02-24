c11c202de471d0b78130f40679c025d6daaabb82
Joshardals <joshuabamidele219@gmail.com>
2025-02-24 01:44:07 +0100

    initial commit

--- app/_components/Interactions.tsx
+++ app/_components/Interactions.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useState } from "react";
 import { ArrowLeft, Settings, ChevronDown } from "lucide-react";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
